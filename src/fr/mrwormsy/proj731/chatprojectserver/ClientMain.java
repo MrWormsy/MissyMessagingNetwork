650f53f290be28a8f05dd8d146eb9b575f02ab3f
MrWormsy <antonin.martin.rosa@gmail.com>
2020-01-27 10:32:16 +0100

    The registry server is now on rosacorp.net:22222

--- src/fr/mrwormsy/proj731/chatprojectserver/ClientMain.java
+++ src/fr/mrwormsy/proj731/chatprojectserver/ClientMain.java
@@ -12,7 +12,7 @@ public class ClientMain {
     public static RemoteClient theClient;
 
     // The address of the regitry server (that will may be rosacorp.net)
-    public static String registryAddress = "localhost";
+    public static String registryAddress = "rosacorp.net";
 
     // Main method
     public static void main(String[] args) throws RemoteException {
